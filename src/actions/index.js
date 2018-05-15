b37d5b6a2525b131536ada18da8306732653fa0b
Dubravko <woose@web.de>
2018-05-15 15:04:10 +0200

    finished creation of action-creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,15 @@
 import API_KEY from "../secret/secret";
+import axios from "axios";
 
 const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},de`;
+  const request = axios.get(url);
 
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }

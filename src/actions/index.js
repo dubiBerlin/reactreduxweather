70c7575a10eca903b05227c3041ad98ef8dbc651
Dubravko <woose@web.de>
2018-05-15 15:28:34 +0200

    connect Action-Creator with SearchBar component

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,8 @@ import axios from "axios";
 const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
+// Der ActionCreator führt das http request durch und gibt ein Action-Objekt zurück
+// was den Action Type und das response objekt vom request enthält.
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},de`;
   const request = axios.get(url);
